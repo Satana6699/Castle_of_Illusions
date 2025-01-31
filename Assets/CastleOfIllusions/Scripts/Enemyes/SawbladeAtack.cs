bc79ec8a33a7dbb6705675bdb00006e76609b0e6
Satana <mbelikeev3@gmail.com>
2025-02-01 00:59:54 +0300

    sawblade have damage

---
+++ Assets/CastleOfIllusions/Scripts/Enemyes/SawbladeAtack.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CastleOfIllusions.Scripts;
+using UnityEngine;
+
+public class SawbladeAtack : MonoBehaviour
+{
+    [SerializeField] private float damage = 10f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
+    }
+}
